4623a483723a4b546b465d90dd40e02ddb65913d
pavana-nr <pavana-nr@users.noreply.github.com>
2025-04-06 12:24:42 +0000

    deploy: 859112f912545244990545187b4e82da777b8102

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
-"index.html": "ed46218401fce61d472d46c02129e534",
-"/": "ed46218401fce61d472d46c02129e534",
+"index.html": "c67a21cd823ffe9a0b16b41e1ee3e404",
+"/": "c67a21cd823ffe9a0b16b41e1ee3e404",
 "assets/NOTICES": "8d09b5c03e8e946b535931860645f552",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "f81685e74559dec6b64a9fd2961a24ac",
@@ -35,7 +35,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "6b35940c44c4d5993c68e5a4bf7f22d4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "a7311a1dce35a9a1039c86bdadd26b3d"};
+"flutter_bootstrap.js": "5d5c786408cf06d63ff326e73a7d23c8"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
